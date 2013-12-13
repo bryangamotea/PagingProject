7dc95689d1d959c5ae3d296d0c0ccfbf7efda455
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-13 11:05:00 +0800

    Finished paging project.

--- paging.js
+++ paging.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
-
+	var maxpage = $("ul").length;
+	page = 1;
+	console.log(page);
 	function navigatePage(bry) {
-		console.log(bry);
 		$("ul").removeClass("current");
 		$("ul:nth-child(" + bry + ")").addClass("current");
+		page = bry;
 	}
 
 	$(".navigation a").click(function(){
-		navigatePage($(this).data("target"));	
-	});
+			if ($(this).hasClass("prev")) {
+				console.log(page);
+				if (page > 1){
+					page--;
+				}
+				navigatePage(page);	
+			} else if ($(this).hasClass("next")){
+				console.log(page);
+				if (page < maxpage) {
+					page++;
+				}
+				navigatePage(page);
+			} else {
+				// console.log($(this).data("target"));
+				navigatePage($(this).data("target"));
+			}
 
+		});
 });
