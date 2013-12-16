5a2167cbf2fc2ef27303e441c56bf296d40c71bc
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-16 10:45:57 +0800

    Used Ajax.

--- paging.js
+++ paging.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
 	
 	var maxpage = $(".num-page").length;
-	console.log(maxpage);
 	page = 1;
 	navigatePage(page);
 	
 	function navigatePage(bry) {
-
 		page = bry;
 		$.ajax({
 			type: "GET"
+			// , url: "http://192.168.1.101:8080/data"
 			, url: "http://localhost:8080/data"
 			, data : {page:page}
 		})
@@ -21,12 +20,24 @@ $(document).ready(function(){
 				var result = msg.data[i].item;
 				$("<li>" + result + "</li>").appendTo(".current")
 			}
+			// $("ul").removeClass("green");
+
+			if (page % 2 == 0) {
+				$("ul").removeClass("red");
+				$("ul").addClass("green");
+
+			} else {
+
+				$("ul").removeClass("green");
+				$("ul").addClass("red");
+
+			}
 
 		});
 
 	}
 
-	$(".navigation a").click(function(){
+	$(".navigation a").click(function(e){
 		if ($(this).hasClass("prev")) {
 			console.log(page);
 			if (page > 1){
@@ -45,8 +56,8 @@ $(document).ready(function(){
 			navigatePage($(this).data("target"));
 			}
 
+			e.stopPropagation();
+
 	});
-
-
 
 });
